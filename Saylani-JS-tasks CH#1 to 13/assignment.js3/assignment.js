03ada0d11f82776b70fedd1a44490eb2daa1f815
waqarsheikhs <78560090+waqarsheikhs@users.noreply.github.com>
2021-06-13 20:11:54 +0500

    Add files via upload

---
+++ Saylani-JS-tasks CH#1 to 13/assignment.js3/assignment.js
@@ -0,0 +1,179 @@
+                                        // Chapter 12 to 13
+// Question 1
+// var A=+prompt("ENTER NUMBER") 
+// if(A<0){
+//     alert("Number is Negative")
+// }
+// else if(A>0){
+//     alert("Number is Positive")
+// }
+// else{
+//     alert("Number is Zero")
+// }
+
+
+
+// Question 2
+// var A=+prompt("ENTER NUMBER")
+// var B=+prompt("ENTER NUMBER")
+// if(A>B){
+//     alert(A + " is larger")
+// }
+// else if(A<B){
+//     alert(B + " is larger")
+// }
+// else
+// {
+//     alert("Number is Equal")
+// }
+
+
+
+// Question 3
+// var vovel=["A","a","E","e","I","i","O","o","U","u"]
+// var A=prompt("ENTER CHARACTER")
+// if(A==vovel){
+//     alert("True Character is Vovel")
+// }
+// else{
+//     alert("False  Character is Not Vovel")
+// }
+
+
+
+// Question 4
+// var enter=prompt("ENTER PASSWORD")
+// var pass="BASIT"
+// if(enter == " ")
+// {
+//     alert(" “ Please enter your password” ")
+// }
+// else if(enter == pass)
+// {
+//     alert(" “Correct! The password you entered matches the original password” ")
+// }
+// else
+// {
+//     alert(" “Incorrect password” ")
+// }
+
+
+
+// Question 5
+// var greeting;
+// var hour = 13;
+// if (hour < 18)
+// {
+// document.write("Good day")
+// }
+// else
+// {
+// document.write("Good evening")
+// }
+
+
+
+// Question 6
+// var time=+prompt("ENTER TIME")
+// if(time==1)
+// {
+//     document.write("1 AM")
+// }
+// else if(time==2)
+// {
+//     document.write("2 AM")
+// }
+// else if(time==3)
+// {
+//     document.write("3 AM")
+// }
+// else if(time==4)
+// {
+//     document.write("4 AM")
+// }
+// else if(time==5)
+// {
+//     document.write("5 AM")
+// }
+// else if(time==6)
+// {
+//     document.write("6 AM")
+// }
+// else if(time==7)
+// {
+//     document.write("7 AM")
+// }
+// else if(time==8)
+// {
+//     document.write("8 AM")
+// }
+// else if(time==9)
+// {
+//     document.write("9 AM")
+// }
+// else if(time==10)
+// {
+//     document.write("10 AM")
+// }
+// else if(time==11)
+// {
+//     document.write("11 AM")
+// }
+// else if(time==12)
+// {
+//     document.write("12 AM")
+// }
+// else if(time==13)
+// {
+//     document.write("1 PM")
+// }
+// else if(time==14)
+// {
+//     document.write("2 PM")
+// }
+// else if(time==15)
+// {
+//     document.write("3 PM")
+// }
+// else if(time==16)
+// {
+//     document.write("4 PM")
+// }
+// else if(time==17)
+// {
+//     document.write("5 PM")
+// }
+// else if(time==18)
+// {
+//     document.write("6 PM")
+// }
+// else if(time==19)
+// {
+//     document.write("7 PM")
+// }
+// else if(time==20)
+// {
+//     document.write("8 PM")
+// }
+// else if(time==21)
+// {
+//     document.write("9 PM")
+// }
+// else if(time==22)
+// {
+//     document.write("10 PM")
+// }
+// else if(time==23)
+// {
+//     document.write("11 PM")
+// }
+// else if(time==24)
+// {
+//     document.write("12 PM")
+// }
+// else{
+//     document.write("ERROE")
+// }
+
+
+
